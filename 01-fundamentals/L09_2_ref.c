9c491fd286e023d0304c0a9ed880ce036527280b
Disha Srivastava <thisisdishasrivastava@gmail.com>
2026-07-29 16:02:40 +0530

    Update L09_2_ref.c

--- 01-fundamentals/L09_2_ref.c
+++ 01-fundamentals/L09_2_ref.c
@@ -3,6 +3,6 @@
 int external_variable = 5;
 
 // external_variable needs to be a global variable.
-// Only then can it be accessed as the extern variable used in L9_2_externvar.c
+// Only then can it be accessed as the extern variable declared in L09_2_externvar.c
 // There must be no "main" function in this document. 
 // Having a "main" function in both the documents confuses the compiler when it combines both files.
