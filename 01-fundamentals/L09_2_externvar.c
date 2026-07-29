1dbe7dfa0a8ce383ffb76ab037410d67dfe9fe11
Disha Srivastava <thisisdishasrivastava@gmail.com>
2026-07-29 15:59:49 +0530

    Update L09_2_externvar.c

--- 01-fundamentals/L09_2_externvar.c
+++ 01-fundamentals/L09_2_externvar.c
@@ -8,10 +8,10 @@ int main()
 }
 
 /*
-NOTE: there shouldn't be a main function in each of the 2 files you are combining!
+NOTE: there shouldn't be a main function in both of the 2 files you are combining!
 This leads to an error!
 Combine the files L9_2_externvar.c and L9_2_ref.c in the following command:
-code L9_2_externvar.c
-gcc L9_2_externvar.c L9_2_ref.c -o L9
+code L09_2_externvar.c
+gcc L09_2_externvar.c L09_2_ref.c -o L09
 ./L9
 */
